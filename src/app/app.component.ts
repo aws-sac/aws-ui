2939b1dceba5fad7c0b21d9f6ebe0c8828ad64be
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2019-01-14 15:25:44 +0200

    Add paddings and fix url

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,7 +21,7 @@ export class AppComponent {
   }
 
   query1(): any {
-    let apiURL = `http://fuseki-aws.herokuapp.com/meds/query?query=PREFIX+pm%3A%3Chttps%3A%2F%2Ffuseki-aws.herokuapp.com%2FPreturiMedicamente.owl%23%3E%0D%0APREFIX+xsd%3A+%3Chttp%3A%2F%2Fwww.w3.org%2F2001%2FXMLSchema%23%3E%0D%0ASELECT+%3Fentity+%3Fresult%0D%0AWHERE+%7B%0D%0A++++%3Fentity+pm%3ADCI+%3Fresult+.%0D%0A++++%3Fentity+pm%3APretAm+%3Fprice+.%0D%0A++++FILTER%28%3Fresult+%3D+%22OLANZAPINUM%22%29%0D%0A++%7D%0D%0AORDER+BY+ASC%28xsd%3Afloat%28%3Fprice%29%29%0D%0ALIMIT+1&output=json&stylesheet=%2Fxml-to-html.xsl`;
+    let apiURL = `https://fuseki-aws.herokuapp.com/meds/query?query=PREFIX+pm%3A%3Chttps%3A%2F%2Ffuseki-aws.herokuapp.com%2FPreturiMedicamente.owl%23%3E%0D%0APREFIX+xsd%3A+%3Chttp%3A%2F%2Fwww.w3.org%2F2001%2FXMLSchema%23%3E%0D%0ASELECT+%3Fentity+%3Fresult%0D%0AWHERE+%7B%0D%0A++++%3Fentity+pm%3ADCI+%3Fresult+.%0D%0A++++%3Fentity+pm%3APretAm+%3Fprice+.%0D%0A++++FILTER%28%3Fresult+%3D+%22OLANZAPINUM%22%29%0D%0A++%7D%0D%0AORDER+BY+ASC%28xsd%3Afloat%28%3Fprice%29%29%0D%0ALIMIT+1&output=json&stylesheet=%2Fxml-to-html.xsl`;
 
     return this.http.get(apiURL)
       .subscribe(data => {
